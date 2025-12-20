d6ac80b1ad0f6a3b09adb3e662c561298c58e435
viktr <chukwuemekavictor693@gmail.com>
2025-12-20 10:15:31 +0100

    Add site health check command

---
+++ app/Notifications/Sitedown.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class Sitedown extends Notification
+{
+    use Queueable;
+
+    public  $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    //->line('The introduction to the notification.')
+                    //->action('Notification Action', url('/'))
+                    ->subject("aquaPush Alert: Site/DB Issue")
+                    ->line($this->message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
